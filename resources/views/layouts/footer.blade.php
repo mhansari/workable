e8d74586eb4c73ecbaa2f1b3385d21277147714d
M. H Ansari <mhansari@vrhiring.com>
2017-05-11 17:08:19 +0500

    updates

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="">
     <div class="col-md-8 col-md-offset-2">
     	<div class="col-md-12 text-center small" style="margin-top:2%">
-    		Copyright ©{{date("Y")}} CareerJin - All Rights Reserved. 
+    		Copyright ©{{date("Y")}} JobStreet.pk - All Rights Reserved. 
     	</div>
     	
     </div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Jobs in Pakistan - CareerJin</title>
+        <meta name="keywords" content="Jobs in Pakistan, Jobs Pakistan, Jobs, Careers, Recruitment, Employment, Hiring, Banking, CVs, Career, Finance, IT, Marketing, Online Jobs, Opportunity,Pakistan, Resume, Work, naukri, rizq, Rozi" />
+        <meta name="description" content="Find best Jobs in Pakistan, jobs listings and job opportunities on CareerJin.com. Browse more and apply for free! CareerJin.com is Pakistan's leading job website where companies are posting jobs" /><link rel="canonical" href="https://www.careerjin.com/" />
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="CareerJin.com"/>
+        <meta property="fb:app_id" content="108372302535151"/>
+        <meta property="og:title" content="Jobs in Pakistan - CareerJin.com"/>
+        <meta property="og:url" content="//www.careerjin.com/"/>
+        <meta property="og:description" content="Find best Jobs in Pakistan, jobs listings and job opportunities on CareerJin.com. Browse and apply for free! ROZEE.PK is Pakistan's leading job website where companies are posting jobs"/>
+        <meta property="og:image" content="http://careerjin.com/images/logo.png" />
+        <!-- Fonts -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+        <!-- Styles -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+        {{-- <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet"> --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+          crossorigin="anonymous"></script>
+        <link href="{{asset('css/fileinput.min.css')}}" media="all" rel="stylesheet" type="text/css" />
+        <link href="{{asset('css/style-responsive.css')}}" media="all" rel="stylesheet" type="text/css" />
+        <!-- the main fileinput plugin file -->
+        <script src="{{asset('js/fileinput.min.js')}}"></script>
+        <!-- include summernote css/js-->
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
+        <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
+        <script src="{{ asset('js/jquery.maskedinput.min.js')}}"></script>
+        <link href="{{ asset('css/bootstrap-treeview.min.css')}}" rel="stylesheet">
+        <script src="{{ asset('js/bootstrap-treeview.min.js')}}"></script>
+        <script src="{{asset('/vendor/unisharp/laravel-ckeditor/ckeditor.js')}}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
+        <link href="{{ URL::asset('css/tabs.css') }}" rel="stylesheet">
+        <style>
+            body {
+            font-family: 'Lato';
+            }
+
+            .fa-btn {
+            margin-right: 6px;
+            }
+        </style>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ asset('css/build.css') }}"/>
+        <link href="{{ URL::asset('css/dropdown.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('css/carousel.css') }}" rel="stylesheet">
+    </head>
+    <body id="app-layout">
+        <nav class="navbar navbar-default header-bar-borders">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Branding Image -->
+                    <a class="navbar-brand margin-logo" href="{{ url('/') }}">
+                        <img src="{{ URL::asset('images/logo.png') }}" alt="CareerJin.com"/>
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+
+        <ul class="nav navbar-nav">
+            <li class="dropdown dropdown-large">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Browse by City <b class="caret"></b></a>
+                
+                <ul class="dropdown-menu dropdown-menu-large row">
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Glyphicons</li>
+                            <li><a href="#">Available glyphs</a></li>
+                            <li class="disabled"><a href="#">How to use</a></li>
+                            <li><a href="#">Examples</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Dropdowns</li>
+                            <li><a href="#">Example</a></li>
+                            <li><a href="#">Aligninment options</a></li>
+                            <li><a href="#">Headers</a></li>
+                            <li><a href="#">Disabled menu items</a></li>
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Button groups</li>
+                            <li><a href="#">Basic example</a></li>
+                            <li><a href="#">Button toolbar</a></li>
+                            <li><a href="#">Sizing</a></li>
+                            <li><a href="#">Nesting</a></li>
+                            <li><a href="#">Vertical variation</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Button dropdowns</li>
+                            <li><a href="#">Single button dropdowns</a></li>
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Input groups</li>
+                            <li><a href="#">Basic example</a></li>
+                            <li><a href="#">Sizing</a></li>
+                            <li><a href="#">Checkboxes and radio addons</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Navs</li>
+                            <li><a href="#">Tabs</a></li>
+                            <li><a href="#">Pills</a></li>
+                            <li><a href="#">Justified</a></li>
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Navbar</li>
+                            <li><a href="#">Default navbar</a></li>
+                            <li><a href="#">Buttons</a></li>
+                            <li><a href="#">Text</a></li>
+                            <li><a href="#">Non-nav links</a></li>
+                            <li><a href="#">Component alignment</a></li>
+                            <li><a href="#">Fixed to top</a></li>
+                            <li><a href="#">Fixed to bottom</a></li>
+                            <li><a href="#">Static top</a></li>
+                            <li><a href="#">Inverted navbar</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                
+            </li>
+        </ul>
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        <li class="{{ strpos(\Route::getCurrentRoute()->getPath(),'seekers')?'active c':''}}"><a href="{{ asset($country . '/seekers/dashboard') }}">Job Seekers</a></li>
+                        <li  class="{{ strpos(\Route::getCurrentRoute()->getPath(),'employers')?'active c':''}}"><a href="{{ asset($country . '/employers/dashboard') }}">Employers</a></li>
+                    </ul>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ asset($country . '/account/create') }}"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                            <li><a href="{{asset($country . '/account/login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    <span class="glyphicon glyphicon-cog"></span> Manage
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    
+                                    <li><a href="update_company"><i class="glyphicon glyphicon-pencil"></i>  Change Password</a></li>
+                                    <li><a href="{{ url($country . '/account/logout') }}"><i class="fa fa-btn fa-sign-out"></i>  Logout</a></li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div id="holder">
+            <div id="body">
+                <div class="container">
+                    <div class="row">                 
+                        <div class="col-md-12">
+                               <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Example headline.</h1>
+              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="images/lahore.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->                               
+                        </div>                
+                    </div>
+                    <div class="row">
+                    asdashdasdas
+                    dasd
+                    as
+                    dasd
+                    as
+                    das
+                    d
+                    a
+                    das
+                    d
+                    as
+                    das
+                    d
+                    as
+                    das
+
+                    sad
+                    as
+                    das
+                    d
+                    a
+                    das
+                    d
+                    asd
+                    as
+                    das
+                    d
+                    asd
+                    as
+                    das
+                    d
+                    </div>
+                </div>
+            </div>
+            <div id="footer">
+             
+            </div> 
+        </div>    
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.js"></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-multiselect.css') }}" type="text/css">
+        <script type="text/javascript" src="{{ URL::asset('js/bootstrap-multiselect.js') }}"></script>
+        <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+        {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+        <script>
+        $('.carousel').carousel();
+        </script>
+    </body>
+</html>
