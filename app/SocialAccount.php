58a4b05fdc763434fb5b0798424a25ba358a23af
M. H Ansari <mhansari@vrhiring.com>
2017-05-16 18:30:31 +0500

    .

--- app/SocialAccount.php
+++ app/SocialAccount.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialAccount extends Model
 {
-    protected $fillable = ['first_name','user_id', 'provider_user_id', 'provider'];
+    protected $fillable = ['first_name','email','user_id', 'provider_user_id', 'provider'];
 
     public function user()
     {
