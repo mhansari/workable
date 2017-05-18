345925babb24b6165e788a4434605a3c02586bbc
M. H Ansari <mhansari@vrhiring.com>
2017-05-19 01:12:00 +0500

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
 
     public function fb(\SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb2 )
     {
-    $user = Socialite::with('facebook')->user();
+    $user = Socialite::driver('facebook')->user();
 
 echo $user->token;
 
