23e15af7f04ba638acf98697fb2f7c7a5c2f3ab3
M. H Ansari <mhansari@vrhiring.com>
2017-05-19 00:23:12 +0500

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,12 +42,68 @@ class HomeController extends Controller
 
     public function fb()
     {
+// start session
+session_start();
 
-        $helper = Facebook::getRedirectLoginHelper(); 
-        $accessToken = $helper->getAccessToken();  
-        echo $accessToken;
-     // …is identical to writing this
-Facebook::get('/me', $accessToken);
+// init app with app id and secret
+FacebookSession::setDefaultApplication( 'xxx','yyy' );
+
+// login helper with redirect_uri
+$helper = new FacebookRedirectLoginHelper( 'http://yourwebsite.com/app/' );
+
+// see if a existing session exists
+if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
+  // create new session from saved access_token
+  $session = new FacebookSession( $_SESSION['fb_token'] );
+
+  // validate the access_token to make sure it's still valid
+  try {
+    if ( !$session->validate() ) {
+      $session = null;
+    }
+  } catch ( Exception $e ) {
+    // catch any exceptions
+    $session = null;
+  }
+
+} else {
+  // no session exists
+
+  try {
+    $session = $helper->getSessionFromRedirect();
+  } catch( FacebookRequestException $ex ) {
+    // When Facebook returns an error
+  } catch( Exception $ex ) {
+    // When validation fails or other local issues
+    echo $ex->message;
+  }
+
+}
+
+// see if we have a session
+if ( isset( $session ) ) {
+
+  // save the session
+  $_SESSION['fb_token'] = $session->getToken();
+  // create a session using saved token or the new one we generated at login
+  $session = new FacebookSession( $session->getToken() );
+
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject()->asArray();
+
+  // print profile data
+  echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
+
+  // print logout url using session and redirect_uri (logout.php page should destroy the session)
+  echo '<a href="' . $helper->getLogoutUrl( $session, 'http://yourwebsite.com/app/logout.php' ) . '">Logout</a>';
+
+} else {
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '">Login</a>';
+}
     }
     public function welcome($country)
     {
