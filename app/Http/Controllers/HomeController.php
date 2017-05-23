02d3ae5cf3e880b72a5cf7695b05cdf155d146ff
M. H Ansari <mhansari@vrhiring.com>
2017-05-23 19:43:13 +0500

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -45,7 +45,7 @@ class HomeController extends Controller
     {
     $user = Socialite::driver('facebook')->user();
 
-echo $user->token;
+print_r($user);
 
     }
     public function welcome($country)
