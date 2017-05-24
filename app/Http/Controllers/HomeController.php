ef2e45033165cad308724a6ce8a624abafe176d0
M. H Ansari <mhansari@vrhiring.com>
2017-05-24 19:49:21 +0500

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,7 +54,7 @@ class HomeController extends Controller
     {
         $token = $fb2->getAccessTokenFromRedirect();
         $fb2->setDefaultAccessToken($token);
-        $response = $fb2->post('/299010880556401/feed',['message'=>'hi']);
+        $response = $fb2->get('/299010880556401/?fields=access_token');
         print_r($response);
     }
     public function welcome($country)
