4109b1aa6139ab3eeccd2da658f948528de499d8
M. H Ansari <mhansari@vrhiring.com>
2017-05-24 18:59:14 +0500

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,50 +47,9 @@ class HomeController extends Controller
         // Obtain an access token.
 
         $token = $fb2->getAccessTokenFromRedirect();
-        if (! $token) {
-            // Get the redirect helper
-            $helper = $fb2->getRedirectLoginHelper();
-
-            if (! $helper->getError()) {
-                abort(403, 'Unauthorized action.');
-            }
-
-            // User denied the request
-            dd(
-                $helper->getError(),
-                $helper->getErrorCode(),
-                $helper->getErrorReason(),
-                $helper->getErrorDescription()
-            );
-        }
-
-        if (! $token->isLongLived()) {
-            // OAuth 2.0 client handler
-            $oauth_client = $fb2->getOAuth2Client();
-
-            // Extend the access token.
-            try {
-                $token = $oauth_client->getLongLivedAccessToken($token);
-            } catch (Facebook\Exceptions\FacebookSDKException $e) {
-                dd($e->getMessage());
-            }
-        }
+        
 echo $token;
-        $fb2->setDefaultAccessToken($token);
-
-        // Save for later
-        Session::put('fb_user_access_token', (string) $token);
-
-        // Get basic info on the user from Facebook.
-        try {
-            $response = $fb2->get('/me?fields=id,name,email');
-        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-            dd($e->getMessage());
-        }
-
-        // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-        $facebook_user = $response->getGraphUser();
-        print_r($facebook_user);
+        
     }
     public function welcome($country)
     {
