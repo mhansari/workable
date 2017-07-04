41742b9e1576532a933f6cbbbb1d00dbad796b82
M. H Ansari <mhansari@vrhiring.com>
2017-07-04 20:35:49 +0500

    .

--- modules/Employers/Http/Controllers/CompanyInfoController.php
+++ modules/Employers/Http/Controllers/CompanyInfoController.php
@@ -67,7 +67,7 @@ class CompanyInfoController extends Controller {
 	{
 		$obj = CompanyInfo::find(Auth::user()->id);
 		$obj->company_name	=	$request->input('company_name');
-		print_r($request);
+		print_r($request->all());
 		 $obj->about_company = 		$request->input('about_company');
 		$obj->designation	=	$request->input('designation');
 		$obj->country_id	=	$request->input('CountryID');
