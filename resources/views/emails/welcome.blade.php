5c829d0c3116dfe19e6a8041a989bafc9ee9355a
M. H Ansari <mhansari@vrhiring.com>
2017-07-04 13:48:12 +0500

    .

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,9 @@
+<p>Hello {{ $name }},</p>
+<p></p>
+<p>Thanks for signing up on jobstreet.pk, please use following credentials to login</p>
+
+<p>Email : {{$email}}</p>
+<p>Password : {{$pwd}}</p>
+<p></p>
+<p>Thanks & Regards,</p>
+<p>JobStreet.pk support</p>
